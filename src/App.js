b983ac509cbb1fb1a84a8cc664d70cb6263258a6
call-n <callenilsson2001@gmail.com>
2022-07-19 09:29:36 +0200

    final changes to the custom hook

--- src/App.js
+++ src/App.js
@@ -1,25 +1,8 @@
-import { useEffect, useState } from 'react'
-
-// Firebase imports
-import { db } from './Firebase/config'
-import { collection, getDocs } from 'firebase/firestore'
+import { useCollection } from './hooks/useCollection'
 
 function App() {
-  const [sodas, setSodas] = useState(null)
-
-  useEffect(() => {
-    const ref = collection(db, 'sodas')
-
-    getDocs(ref)
-      .then((snapshot) => {
-        let results = []
-        snapshot.docs.forEach(doc => {
-          results.push({id: doc.id, ...doc.data()})
-        })
-        setSodas(results)
-      })
-  }, [])
-
+  const { documents: sodas } = useCollection('sodas')
+  
   console.log(sodas)
 
   return (
